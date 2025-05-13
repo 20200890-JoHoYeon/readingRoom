876d0238ad4b34b4abb940a0dff5f8510744a9cf
DESKTOP-3VDE5ID\USER <akftjd100@gmail.com>
2025-05-13 16:33:51 +0900

    [생성] C# TCPClient

---
+++ C#/Socket/TCPClient/SocketTCPClient/TcpClient.cs
@@ -0,0 +1,58 @@
+﻿// TCP Client
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpClientApp
+{
+    static void Main()
+    {
+        string message = "";
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+
+        while (message != "0") {
+           
+            Console.Write("서버에 보낼 값(종료는 0): ");
+            message = Console.ReadLine();
+            //string message = "Hello TCP Server!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);  // 메시지 전송
+
+            byte[] buffer = new byte[1024];
+            int bytes = stream.Read(buffer, 0, buffer.Length);  // 응답 수신
+
+            Console.WriteLine("서버 응답: " + Encoding.UTF8.GetString(buffer, 0, bytes));
+            
+        }
+        client.Close();
+
+    }
+}
+
+
+// TCP Client 원본
+/*
+using System;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpClientApp
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+
+        string message = "Hello TCP Server!";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);  // 메시지 전송
+
+        byte[] buffer = new byte[1024];
+        int bytes = stream.Read(buffer, 0, buffer.Length);  // 응답 수신
+        Console.WriteLine("서버 응답: " + Encoding.UTF8.GetString(buffer, 0, bytes));
+
+        client.Close();
+    }
+}
+*/

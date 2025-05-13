6baa2a3115da52c4454239b11810e9b94a17d16f
DESKTOP-3VDE5ID\USER <akftjd100@gmail.com>
2025-05-13 17:43:04 +0900

    [수정] C# TCPServer,  TCPClient

--- C#/Socket/TCPClient/SocketTCPClient/TcpClient.cs
+++ C#/Socket/TCPClient/SocketTCPClient/TcpClient.cs
@@ -5,14 +5,49 @@ using System.Text;
 
 class TcpClientApp
 {
+    // TCP Client (반복 송수신 및 조건 종료 - 쓰고 읽기)
     static void Main()
     {
         string message = "";
         TcpClient client = new TcpClient("127.0.0.1", 5000);
         NetworkStream stream = client.GetStream();
 
-        while (message != "0") {
+        while(true) {
            
+            Console.Write("서버에 보낼 값(종료는 0): ");
+            message = Console.ReadLine();
+            //string message = "Hello TCP Server!";
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);  // 메시지 전송
+            Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] 서버로 데이터 전송 완료");
+
+            if (message == "0")
+                break;  // 바로 종료
+
+
+            byte[] buffer = new byte[1024];
+            int bytes = stream.Read(buffer, 0, buffer.Length);  // 응답 수신
+
+            Console.WriteLine("서버 응답: " + Encoding.UTF8.GetString(buffer, 0, bytes));
+            Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] 서버 응답 전송 완료");
+        }
+        client.Close();
+
+    }
+}
+// TCP Client (반복 송신 및 조건 종료 - 쓰기)
+/*
+class TcpClientApp
+{
+    static void Main()
+    {
+        string message = "";
+        TcpClient client = new TcpClient("127.0.0.1", 5000);
+        NetworkStream stream = client.GetStream();
+
+        while (message != "0")
+        {
+
             Console.Write("서버에 보낼 값(종료는 0): ");
             message = Console.ReadLine();
             //string message = "Hello TCP Server!";
@@ -23,13 +58,13 @@ class TcpClientApp
             int bytes = stream.Read(buffer, 0, buffer.Length);  // 응답 수신
 
             Console.WriteLine("서버 응답: " + Encoding.UTF8.GetString(buffer, 0, bytes));
-            
+
         }
         client.Close();
 
     }
 }
-
+*/
 
 // TCP Client 원본
 /*
--- C#/Socket/TCPServer/SocketTEST/TcpServer.cs
+++ C#/Socket/TCPServer/SocketTEST/TcpServer.cs
@@ -1,9 +1,10 @@
-﻿// TCP Server (연결 지향)
+﻿// TCP Server 
 using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
+// TCP Server (반복 송수신 및 조건 종료 - 읽고 쓰기)
 class TcpServer
 {
     static void Main()
@@ -16,6 +17,50 @@ class TcpServer
         NetworkStream stream = client.GetStream();
         
         string received = "";
+        string message = "";
+        while (true)
+        {
+            byte[] buffer = new byte[1024];
+            int byteCount = stream.Read(buffer, 0, buffer.Length);  // 수신
+            received = Encoding.UTF8.GetString(buffer, 0, byteCount);
+            Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] 클라이언트로부터: " + received);
+
+            if (received == "0")
+            {
+                byte[] endMsg = Encoding.UTF8.GetBytes("서버연결 종료됨.");
+                stream.Write(endMsg, 0, endMsg.Length);
+                break;
+            }
+
+            // 응답 전송
+            Console.Write("서버에 보낼 값: ");
+            message = Console.ReadLine();
+            byte[] response = Encoding.UTF8.GetBytes(message);
+            //byte[] response = Encoding.UTF8.GetBytes("Hello TCP Client!");
+            if (received == "0") response = Encoding.UTF8.GetBytes("서버연결 종료됨.");
+            stream.Write(response, 0, response.Length);
+            Console.WriteLine("[" + DateTime.Now.ToString("HH:mm:ss") + "] 클라이언트로 응답 전송 완료");
+        }
+        Console.WriteLine("클라이언트 연결 종료됨.");
+        client.Close();
+        server.Stop();
+    }
+}
+
+// TCP Server (반복 수신 및 조건 종료 - 읽기)
+/*
+class TcpServer
+{
+    static void Main()
+    {
+
+        TcpListener server = new TcpListener(IPAddress.Any, 5000);
+        server.Start();
+        Console.WriteLine("TCP 서버 시작됨. 클라이언트 대기 중...");
+        TcpClient client = server.AcceptTcpClient();  // 연결 수락
+        NetworkStream stream = client.GetStream();
+
+        string received = "";
         while (received != "0")
         {
             byte[] buffer = new byte[1024];
@@ -27,16 +72,16 @@ class TcpServer
             byte[] response = Encoding.UTF8.GetBytes("Hello TCP Client!");
             stream.Write(response, 0, response.Length);
 
-       
+
         }
         Console.WriteLine("클라이언트 연결 종료됨.");
         client.Close();
         server.Stop();
     }
 }
+*/
 
-
-// TCP Server (연결 지향)
+// TCP Server 원본
 /*
 using System;
 using System.Net;
